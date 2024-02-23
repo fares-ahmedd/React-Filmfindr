3b9587b3e924c8ef66232179efe13f56de0ae813
fares <fares.haliim@gmail.com>
2024-02-23 07:49:33 +0200

    fix issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StartRating from "./starRating";
 import { useMovies } from "./useMovies";
 import { useLocalStorageState } from "./useLocalStorageState";
